f0c381a54f813f2f72ed269cb8ff81b198de9243
Stanisław Janowicz <147522953+StanleJanesz@users.noreply.github.com>
2024-11-27 12:05:44 +0100

    Add files via upload

--- NewEda.py
+++ NewEda.py
@@ -1,34 +1,37 @@
 import pandas as pd
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Load the CSV file
-data = pd.read_csv('C:\\studia\\IML\\features.csv')
 
-# Check the first few rows of the data
+data = pd.read_csv('C:\\studia\\IML\\featuresOld.csv')
+
+
 print(data.head())
 
 
-# Check for missing values
+
 print(data.isnull().sum())
 
-# Get basic statistics of numerical features
+
 print(data.describe())
 
-# Check data types and structure
+
 print(data.info())
 
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Set up the plot
+
 plt.figure(figsize=(12, 8))
 
-# Plot histograms for each numerical feature
-numerical_columns = [ "Mean_R", "Mean_G", "Mean_B", "Mean_Brightness", "Mean_Saturation", "Contrast"]
 
-# Plotting individual histograms
+numerical_columns = [ "Mean_R", "Mean_Brightness",  "Contrast","Homogeneity","Edge_density"]
+
+
 for i, col in enumerate(numerical_columns):
-    plt.subplot(2, 4, i + 1)
+    plt.subplot(2, 3, i + 1)
     sns.histplot(data[col], kde=True, bins=20)
     plt.title(f'Distribution of {col}')
 
@@ -36,36 +39,35 @@ plt.tight_layout()
 plt.show()
 
 
-# Plot the distribution of 'Gender'
 plt.figure(figsize=(8, 6))
 sns.countplot(x='Gender', data=data)
 plt.title('Gender Distribution')
 plt.show()
 
-# Plot the distribution of 'Person'
+
 plt.figure(figsize=(12, 6))
 sns.countplot(x='Person', data=data)
 plt.title('Person Distribution')
-plt.xticks(rotation=90)  # Rotate x-axis labels if there are many unique persons
+plt.xticks(rotation=90)  
 plt.show()
 
-# Plot the distribution of 'Script'
+
 plt.figure(figsize=(12, 6))
 sns.countplot(x='Script', data=data)
 plt.title('Script Distribution')
-plt.xticks(rotation=90)  # Rotate x-axis labels if there are many unique persons
+plt.xticks(rotation=90)  
 plt.show()
-# Compute the correlation matrix
+
 correlation_matrix = data[numerical_columns].corr()
 
-# Plot the heatmap
+
 plt.figure(figsize=(8, 6))
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f')
 plt.title('Correlation Matrix of Numerical Features')
 plt.show()
 
 
-# Box plots for 'Gender' vs numerical features
+
 plt.figure(figsize=(12, 8))
 for i, col in enumerate(numerical_columns):
     plt.subplot(2, 4, i + 1)
@@ -74,80 +76,74 @@ for i, col in enumerate(numerical_columns):
 plt.tight_layout()
 plt.show()
 
-# Box plots for 'Script' vs numerical features
+
 plt.figure(figsize=(12, 8))
 for i, col in enumerate(numerical_columns):
-    plt.subplot(2, 4, i + 1)
+    plt.subplot(2, 3, i + 1)
     sns.boxplot(x='Script', y=col, data=data)
     plt.title(f'Script vs {col}')
 plt.tight_layout()
 plt.show()
 
-# Box plots for 'AudioType' vs numerical features
 plt.figure(figsize=(12, 8))
 for i, col in enumerate(numerical_columns):
-    plt.subplot(2, 4, i + 1)
+    plt.subplot(2, 3, i + 1)
     sns.boxplot(x='AudioType', y=col, data=data)
     plt.title(f'AudioType vs {col}')
 plt.tight_layout()
 plt.show()
 
-# Box plots for 'IsTestSet' vs numerical features
 plt.figure(figsize=(12, 8))
 for i, col in enumerate(numerical_columns):
-    plt.subplot(2, 4, i + 1)
+    plt.subplot(2, 3, i + 1)
     sns.boxplot(x='IsTestSet', y=col, data=data)
     plt.title(f'IsTestSet vs {col}')
 plt.tight_layout()
 plt.show()
 
 
-# Box plots for 'Class' vs numerical features
+
 plt.figure(figsize=(12, 8))
 for i, col in enumerate(numerical_columns):
-    plt.subplot(2, 4, i + 1)
+    plt.subplot(2, 3, i + 1)
     sns.boxplot(x='Class', y=col, data=data)
     plt.title(f'Class vs {col}')
 plt.tight_layout()
 plt.show()
 
-
-# Box plots for 'Person' vs numerical features
 plt.figure(figsize=(12, 8))
 for i, col in enumerate(numerical_columns):
-    plt.subplot(2, 4, i + 1)
+    plt.subplot(2, 3, i + 1)
     sns.boxplot(x='Person', y=col, data=data)
     plt.title(f'Person vs {col}')
 plt.tight_layout()
 plt.show()
 
-# Pairplot for numerical features
 sns.pairplot(data[numerical_columns])
 plt.show()
 
 
-# Countplot of Person vs Gender
 plt.figure(figsize=(12, 6))
 sns.countplot(x='Person', hue='Gender', data=data)
 plt.title('Person vs Gender Distribution')
 plt.xticks(rotation=90)
 plt.show()
 
-# Countplot of Person vs IsTestSet
+
 plt.figure(figsize=(12, 6))
 sns.countplot(x='Person', hue='IsTestSet', data=data)
 plt.title('Person vs IsTestSet Distribution')
 plt.xticks(rotation=90)
 plt.show()
 
-# Countplot of Person vs Gender
+
 plt.figure(figsize=(12, 6))
 sns.countplot(x='AudioType', hue='IsTestSet', data=data)
-plt.title('AudioType vs IsTestSet Distribution')
+plt.title('AudioType   vs IsTestSet Distribution')
 plt.xticks(rotation=90)
 plt.show()
 
-# Countplot of Person vs Gender
+
 plt.figure(figsize=(12, 6))
 sns.countplot(x='Person', hue='IsTestSet', data=data)
 plt.title('Person vs IsTestSet Distribution')
--- createCsv.py
+++ createCsv.py
@@ -2,84 +2,146 @@ import cv2
 import numpy as np
 import csv
 import os
-# Funkcja obliczająca średni kolor obrazu w przestrzeni RGB
+import pandas as pd
+import cv2
+import numpy as np
+from skimage.feature import graycomatrix, graycoprops 
+import matplotlib.pyplot as plt
+
 def mean_color(image):
-    mean_b = np.mean(image[:,:,0])  # Średni kolor niebieski (B)
-    mean_g = np.mean(image[:,:,1])  # Średni kolor zielony (G)
-    mean_r = np.mean(image[:,:,2])  # Średni kolor czerwony (R)
+    mean_b = np.mean(image[:,:,0])  
+    mean_g = np.mean(image[:,:,1])  
+    mean_r = np.mean(image[:,:,2])  
     return mean_r, mean_g, mean_b
 
-# Funkcja obliczająca średnią jasność obrazu
+
 def mean_brightness(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return np.mean(gray_image)
 
-# Funkcja obliczająca średnie nasycenie obrazu w przestrzeni HSV
+
 def mean_saturation(image):
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     mean_s = np.mean(hsv_image[:,:,1])  # Nasycenie (S)
     return mean_s
 
-# Funkcja obliczająca kontrast obrazu
 def contrast(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return np.std(gray_image)
 
-# Funkcja zapisująca cechy do pliku CSV
-def save_features_to_csv(image_path, output_csv):
-    # Wczytanie obrazu
+class_one_indicators = ['f1_', 'f7_', 'f8_', 'm3_', 'm6_', 'm8_'] 
+
+class_one_test = ['f1_script2_cleanraw', 'f8_script3_cleanraw', 'f8_script5_cleanraw', 'm8_script2_cleanraw', 'm8_script3_cleanraw', 'm8_script4_cleanraw', 'f7_script4_ipadflat_confroom1', 'm3_script3_ipadflat_confroom1', 'm6_script1_ipadflat_confroom1', 'm6_script4_ipadflat_confroom1', 'f1_script2_ipadflat_office1', 'f7_script3_ipadflat_office1', 'f7_script4_ipadflat_office1', 'f8_script3_ipadflat_office1', 'm3_script3_ipadflat_office1', 'm3_script4_ipadflat_office1', 'f8_script3_ipad_balcony1', 'm3_script1_ipad_balcony1', 'f8_script4_ipad_bedroom1', 'm3_script2_ipad_bedroom1', 'm6_script3_ipad_bedroom1', 'm6_script5_ipad_bedroom1', 'm8_script3_ipad_bedroom1', 'f7_script2_ipad_confroom1', 'f8_script4_ipad_confroom1', 'm3_script4_ipad_confroom1', 'm6_script2_ipad_confroom1', 'm8_script2_ipad_confroom1', 'm8_script3_ipad_confroom1', 'f1_script3_ipad_confroom2', 'f7_script1_ipad_confroom2', 'f7_script2_ipad_confroom2', 'm3_script5_ipad_confroom2', 'm6_script2_ipad_confroom2', 'm6_script3_ipad_confroom2', 'm8_script1_ipad_confroom2', 'f7_script1_ipad_livingroom1', 'f7_script4_ipad_livingroom1', 'f8_script5_ipad_livingroom1', 'm3_script2_ipad_livingroom1', 'm6_script4_ipad_livingroom1', 'm6_script5_ipad_livingroom1', 'm8_script5_ipad_livingroom1', 'f1_script3_ipad_office1', 'f7_script1_ipad_office1', 'm8_script5_ipad_office1', 'f1_script4_ipad_office2', 'f7_script2_ipad_office2', 'm3_script2_ipad_office2', 'f1_script3_iphone_balcony1', 'f7_script5_iphone_balcony1', 'f8_script2_iphone_balcony1', 'f8_script3_iphone_balcony1', 'm3_script3_iphone_balcony1', 'm6_script3_iphone_balcony1', 'm6_script5_iphone_balcony1', 'f1_script2_iphone_bedroom1', 'f8_script3_iphone_bedroom1', 'f8_script4_iphone_bedroom1', 'm8_script5_iphone_bedroom1']
+
+class_zero_test = ['f10_script2_cleanraw', 'f2_script3_cleanraw', 'f4_script4_cleanraw', 'f5_script1_cleanraw', 'f6_script5_cleanraw', 'f9_script2_cleanraw', 'm2_script5_cleanraw', 'm5_script2_cleanraw', 'f10_script5_ipadflat_confroom1', 'f4_script3_ipadflat_confroom1', 'f9_script3_ipadflat_confroom1', 'm1_script5_ipadflat_confroom1', 'm2_script4_ipadflat_confroom1', 'm4_script2_ipadflat_confroom1', 'm7_script4_ipadflat_confroom1', 'm9_script5_ipadflat_confroom1', 'f10_script4_ipadflat_office1', 'f2_script2_ipadflat_office1', 'f4_script1_ipadflat_office1', 'f5_script1_ipadflat_office1', 'f5_script5_ipadflat_office1', 'f6_script1_ipadflat_office1', 'm10_script1_ipadflat_office1', 'm2_script3_ipadflat_office1', 'm4_script3_ipadflat_office1', 'm7_script2_ipadflat_office1', 'm7_script3_ipadflat_office1', 'm9_script3_ipadflat_office1', 'f10_script3_ipad_balcony1', 'f2_script3_ipad_balcony1', 'f4_script5_ipad_balcony1', 'f5_script1_ipad_balcony1', 'f5_script3_ipad_balcony1', 'f5_script4_ipad_balcony1', 'f6_script4_ipad_balcony1', 'f9_script1_ipad_balcony1', 'f9_script3_ipad_balcony1', 'm10_script3_ipad_balcony1', 'm10_script4_ipad_balcony1', 'm1_script5_ipad_balcony1', 'm9_script4_ipad_balcony1', 'f2_script1_ipad_bedroom1', 'f4_script4_ipad_bedroom1', 'f6_script1_ipad_bedroom1', 'f9_script3_ipad_bedroom1', 'm4_script2_ipad_bedroom1', 'm7_script2_ipad_bedroom1', 'm7_script5_ipad_bedroom1', 'f10_script3_ipad_confroom1', 'f3_script4_ipad_confroom1', 'f5_script5_ipad_confroom1', 'f6_script5_ipad_confroom1', 'm4_script3_ipad_confroom1', 'm9_script4_ipad_confroom1', 'm9_script5_ipad_confroom1', 'f2_script2_ipad_confroom2', 'f4_script3_ipad_confroom2', 'm1_script2_ipad_confroom2', 'm1_script5_ipad_confroom2', 'm5_script5_ipad_confroom2', 'f2_script4_ipad_livingroom1', 'f3_script4_ipad_livingroom1', 'f4_script2_ipad_livingroom1', 'f5_script3_ipad_livingroom1', 'f6_script1_ipad_livingroom1', 'f6_script4_ipad_livingroom1', 'm10_script5_ipad_livingroom1', 'm2_script2_ipad_livingroom1', 'm2_script5_ipad_livingroom1', 'm4_script4_ipad_livingroom1', 'm4_script5_ipad_livingroom1', 'm5_script1_ipad_livingroom1', 'm9_script2_ipad_livingroom1', 'f10_script1_ipad_office1', 'f10_script2_ipad_office1', 'f10_script3_ipad_office1', 'f10_script5_ipad_office1', 'f2_script3_ipad_office1', 'f5_script1_ipad_office1', 'f6_script5_ipad_office1', 'f9_script1_ipad_office1', 'm10_script1_ipad_office1', 'm10_script4_ipad_office1', 'm1_script2_ipad_office1', 'm2_script1_ipad_office1', 'm2_script3_ipad_office1', 'm2_script4_ipad_office1', 'm2_script5_ipad_office1', 'm5_script2_ipad_office1', 'm7_script2_ipad_office1', 'm7_script5_ipad_office1', 'f2_script3_ipad_office2', 'f4_script1_ipad_office2', 'f5_script1_ipad_office2', 'm1_script2_ipad_office2', 'm2_script4_ipad_office2', 'm4_script1_ipad_office2', 'm4_script2_ipad_office2', 'm4_script4_ipad_office2', 'm4_script5_ipad_office2', 'm5_script2_ipad_office2', 'm5_script3_ipad_office2', 'm5_script5_ipad_office2', 'm7_script2_ipad_office2', 'f10_script1_iphone_balcony1', 'f10_script4_iphone_balcony1', 'f2_script3_iphone_balcony1', 'f2_script4_iphone_balcony1', 'f2_script5_iphone_balcony1', 'm1_script2_iphone_balcony1', 'm2_script2_iphone_balcony1', 'm2_script4_iphone_balcony1', 'm4_script4_iphone_balcony1', 'm4_script5_iphone_balcony1', 'm7_script3_iphone_balcony1', 'm7_script4_iphone_balcony1', 'm9_script2_iphone_balcony1', 'm9_script4_iphone_balcony1', 'f2_script5_iphone_bedroom1', 'f4_script3_iphone_bedroom1', 'f5_script2_iphone_bedroom1', 'f5_script4_iphone_bedroom1', 'f6_script2_iphone_bedroom1', 'f6_script3_iphone_bedroom1', 'm10_script3_iphone_bedroom1', 'm10_script4_iphone_bedroom1', 'm1_script2_iphone_bedroom1', 'm4_script3_iphone_bedroom1', 'm4_script5_iphone_bedroom1', 'm9_script3_iphone_bedroom1', 'f3_script4_iphone_livingroom1', 'f3_script5_iphone_livingroom1', 'f4_script2_iphone_livingroom1', 'f4_script5_iphone_livingroom1', 'f6_script1_iphone_livingroom1', 'f9_script2_iphone_livingroom1', 'm1_script2_iphone_livingroom1', 'm2_script4_iphone_livingroom1', 'm4_script4_iphone_livingroom1', 'm7_script1_iphone_livingroom1']
+
+def calculate_texture_homogeneity(image_path):
+
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    
+
+    if len(image.shape) == 3:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+    image = np.uint8(image / np.max(image) * 255)
+
+
+           
+    glcm = graycomatrix(image, distances=[1], angles=[0, np.pi/4, np.pi/2, 3*np.pi/4], symmetric=True, normed=True)
+
+    homogeneity = graycoprops(glcm, prop='homogeneity')
+    
+  
+    average_homogeneity = np.mean(homogeneity)
+    return average_homogeneity
+
+
+def calculate_edge_density(image_path, low_threshold=100, high_threshold=200):
+
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    
+
+    if len(image.shape) == 3:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+
+    edges = cv2.Canny(image, low_threshold, high_threshold)
+    
+
+    edge_pixels = np.count_nonzero(edges)  
+    
+
+    total_pixels = image.size
+
+    edge_density = edge_pixels / total_pixels
+
+    return edge_density
+
+
+
+
+
+
+
+def save_features_to_csv(image_path, output_csv,class_one_indicators,class_one_test,class_zero_test):
+
     image = cv2.imread(image_path)
     
     if image is None:
         print("Błąd wczytywania obrazu.")
         return
     
-    # Obliczenie cech
+
     mean_r, mean_g, mean_b = mean_color(image)
     mean_brightness_value = mean_brightness(image)
     mean_saturation_value = mean_saturation(image)
     contrast_value = contrast(image)
-    
-    # Zapis do pliku CSV  
+    edge_density = calculate_edge_density(image_path, 100, 00)
+    homogeneity = calculate_texture_homogeneity(image_path)
+
     with open(output_csv, mode='a', newline='') as file:
         writer = csv.writer(file)
         
-        # Zapis nagłówka, jeśli plik jest pusty
+
         if file.tell() == 0:
-            writer.writerow(["Image","Person","Gender", "Mean_R", "Mean_G", "Mean_B", "Mean_Brightness", "Mean_Saturation", "Contrast"])
+            writer.writerow(["Image","Person","Gender","Script","AudioType","IsTestSet","Class", "Mean_R", "Mean_G", "Mean_B", "Mean_Brightness", "Mean_Saturation", "Contrast","Homogeneity","Edge_density"])
         split_string = image_path.split("\\")
         photoName = split_string[len(split_string)-1]
         gender = photoName[0]
+        if any(indicator in photoName for indicator in class_one_indicators):
+            PhotoClass = 1
+        else:
+            PhotoClass = 0
+        if any(indicator in photoName for indicator in class_one_test):
+            IsTestSet = 1  
+        elif any(indicator in photoName for indicator in class_zero_test):
+            IsTestSet = 1
+        else:
+            IsTestSet = 0
         if (gender == "f") :
             gender = "Female"
         else :
             gender = "Male"
         Person = photoName.split("_")[0]
-        # Zapis danych obrazu
-        writer.writerow([image_path,Person,gender, mean_r, mean_g, mean_b, mean_brightness_value, mean_saturation_value, contrast_value])
+        script = photoName.split("_")[1]
+        AudioType = photoName.split("_")[2]
+ 
+        writer.writerow([image_path,Person,gender,script,AudioType,IsTestSet ,PhotoClass, mean_r, mean_g, mean_b, mean_brightness_value, mean_saturation_value, contrast_value,homogeneity,edge_density])
     
-   # print(f"Cecha obrazu {image_path} została zapisana.")
 
-# Przykład użycia
-image_paths = ["obraz1.jpg", "obraz2.jpg", "obraz3.jpg"]  # Lista ścieżek do obrazów
-output_csv = "features.csv"  # Nazwa pliku CSV, do którego zapisujemy cechy
-folder_path = 'C:\\studia\\IML\\croped'
-image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp']
+output_csv = "featuresOld.csv"  
+folder_path = 'C:\\studia\\IML\\cropedold'
+image_extensions = ['.jpg',  '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp']
 
-    # List to store image file paths
+
 image_files = []
 i = 0
-    # Loop through all files in the folder
+
 for file in os.listdir(folder_path):
-        # Check if the file has an image extension
+
     if any(file.lower().endswith(ext) for ext in image_extensions):
         i =i+1
-        save_features_to_csv(os.path.join(folder_path, file), output_csv) 
+        save_features_to_csv(os.path.join(folder_path, file), output_csv,class_one_indicators,class_one_test,class_zero_test) 
         print(f"Cecha obrazu {i} została zapisana.")
 
-
-# Example usage
-#folder_path = 'path/to/your/folder'  # Replace with the path to your folder
-
-#for image_path in image_paths:
-   # save_features_to_csv(image_path, output_csv)
